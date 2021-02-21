ad2932290458cf20e0e9c535872f0e20f84305fa
Romanna <amartsogttsogtbaatar@gmail.com>
2021-02-21 18:52:17 +0800

    eeljin onoog uurchilj, toglogchiin eeljiig solih

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Аль тоглогч шоог хаяхыг шийддэг хувьсагч. 1-р тоглогч "0", 2-р тоглогч "1".
 
-activePlayer = 1;
+activePlayer = 0;
 
 
 // Тоглогчдын нийт цуглуулсан оноог хадгалах хувьсагч.
@@ -30,10 +30,33 @@ document.getElementById("current-1").textContent = 0;
 var shooniiZurag = document.querySelector(".dice");
 shooniiZurag.style.display = "none"; 
 
-// Roll-Dice гэсэн товчийг ажилладаг болгоё. "Event"
+
+// Roll-Dice гэсэн товчийг ажилладаг болгоё. "Event listener"
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+    // Шооны санамсаргүй буусан тоог хийнэ. 
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     shooniiZurag.style.display = 'block';
-    shooniiZurag.src = 'dice-' + diceNumber + '.png'
+    shooniiZurag.src = 'dice-' + diceNumber + '.png';
+    // 1-ээс ялгаатай тоо буувал ээлжийн оноо хэсэгт тухайн тоог нэмж өгнө.
+    if(diceNumber !== 1){
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }else{
+        // 1 буусан тохиолдолт ээлжийн оноо устах.
+        roundScore = 0; 
+        document.getElementById('current-' + activePlayer).textContent = 0;
+
+        // 1 буусан тохиолдолд нөгөө тоглогч хаяна.
+        // Идэвхтэй тоглогч 0 байвал 1 болгоно. 1 байвал 0 болгоно.
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        // Шоо хаях тоглогчид улаан цэгийг шилжүүлж өгнө.
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle("active")
+        
+        // Шоог хаяад 1 буусан үед шоог алга болгоно.
+        shooniiZurag.style.display = "none"; 
+
+    } 
 })
