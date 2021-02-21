9ee69bce376dd660e9be9b0afc4dbea38542a911
Romanna <amartsogttsogtbaatar@gmail.com>
2021-02-21 22:04:41 +0800

    onoog tsugluulah, hold tovchiig ajildag bolgoh, 100 hurvel ylagch boldog bolgoh

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 // Аль тоглогч шоог хаяхыг шийддэг хувьсагч. 1-р тоглогч "0", 2-р тоглогч "1".
-
 activePlayer = 0;
 
 
 // Тоглогчдын нийт цуглуулсан оноог хадгалах хувьсагч.
-
 var scores = [0, 0];
 
 
 // Тоглогчийн ээлжиндээ цуглуулад байгаа оноо.
-
 var roundScore = 0;
 
 
 // Шооны буусан тоог хадгалах хувьсагч. 1-6 гэсэн тоо энэ хувьсагчид санамсаргүй байдлаар өгнө.
-
 var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // Программ эхлэхэд. 
@@ -26,13 +22,11 @@ document.getElementById("current-1").textContent = 0;
 
 
 // Шооны зураг хэсэг.
-
 var shooniiZurag = document.querySelector(".dice");
 shooniiZurag.style.display = "none"; 
 
 
 // Roll-Dice гэсэн товчийг ажилладаг болгоё. "Event listener"
-
 document.querySelector('.btn-roll').addEventListener('click', function(){
     // Шооны санамсаргүй буусан тоог хийнэ. 
     var diceNumber = Math.floor(Math.random() * 6) + 1;
@@ -43,20 +37,42 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore = roundScore + diceNumber;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     }else{
-        // 1 буусан тохиолдолт ээлжийн оноо устах.
-        roundScore = 0; 
-        document.getElementById('current-' + activePlayer).textContent = 0;
-
-        // 1 буусан тохиолдолд нөгөө тоглогч хаяна.
-        // Идэвхтэй тоглогч 0 байвал 1 болгоно. 1 байвал 0 болгоно.
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-
-        // Шоо хаях тоглогчид улаан цэгийг шилжүүлж өгнө.
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle("active")
-        
-        // Шоог хаяад 1 буусан үед шоог алга болгоно.
-        shooniiZurag.style.display = "none"; 
-
+        shooHayahErh();
     } 
-})
+});
+
+
+// Ээлжиндээ цуглуулсан оноогоо ерөнхий онооруу шилжүүлэх. HOLD товчоо ажлуулах.
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // Уг тоглогчийн цуглуулсан оноог нийт оноонд нь нэмж өгнө.
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Уг тоглогчийг хожсон үгүйг шалгах.
+    if(scores[activePlayer] >= 100){
+        // Ялсан хүний нэрийг WINNER болгоно
+        document.getElementById('name-' + activePlayer).textContent = 'Winner';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+    }else{
+        shooHayahErh();
+    }
+});
+
+
+// Тоглогчийн Шоо хаях эрхийг дараачийн тоглогчид өгнө. 
+function shooHayahErh(){    
+    // 1 буусан тохиолдолт ээлжийн оноо устах.
+    roundScore = 0; 
+    document.getElementById('current-' + activePlayer).textContent = 0;
+
+    // 1 буусан тохиолдолд нөгөө тоглогч хаяна.
+    // Идэвхтэй тоглогч 0 байвал 1 болгоно. 1 байвал 0 болгоно.
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    // Шоо хаях тоглогчид улаан цэгийг шилжүүлж өгнө.
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle("active")
+    
+    // Шоог хаяад 1 буусан үед шоог алга болгоно.
+    shooniiZurag.style.display = "none"; }
